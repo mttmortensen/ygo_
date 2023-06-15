02489b7e563218b866cf8404d94c6d0aec3fa481
Matt Mortensen <mtt.mortensen527@gmail.com>
2023-06-15 16:52:25 -0600

    flattened out meta deck data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from ac_api_call import ygo_ac_call
-from db_utils import create_connection, check_table_exists, count_rows_in_table, insert_data, fetch_all_card_data
-from pandas_data_handle import convert_to_dataframe, fill_missing_values, format_numeric_columns  # Import the new functions
+from ac_api_call import ygo_ac_call # Import the new functions
+from db_utils import create_connection, check_table_exists, count_rows_in_table, insert_data, fetch_all_card_data # Import the new functions
+from pandas_data_handle import convert_to_dataframe, fill_missing_values, format_numeric_columns, flatten_deck_data  # Import the new functions
 from md_scrape_call import get_deck_links, get_deck_list  # Import the new functions
 
 # establish connection to database
@@ -24,23 +24,31 @@ if connection:
         insert_data(connection, data)
 
     # Fetch all card data from the database
-    rows, columns = fetch_all_card_data()
+rows, columns = fetch_all_card_data()
 
-    # Convert the data to a pandas DataFrame
-    df = convert_to_dataframe(rows, columns)
+# Convert the data to a pandas DataFrame
+df = convert_to_dataframe(rows, columns)
 
-    # Fill missing values
-    df = fill_missing_values(df)
+# Fill missing values
+df = fill_missing_values(df)
 
-    # Format the Numeric columns
-    numeric_columns = ['atk', 'def', 'level', 'linkval', 'scale']
-    df = format_numeric_columns(df,numeric_columns)
-    print(df.head())
+# Format the Numeric columns
+numeric_columns = ['atk', 'def', 'level', 'linkval', 'scale']
+df = format_numeric_columns(df,numeric_columns)
+print(df.head())
 
-    # Now df is a pandas DataFrame with no missing values, ready for the next steps
+# Now df is a pandas DataFrame with no missing values, ready for the next steps
 
-    # Scrape deck data
-    print("Starting to scrape deck data...")
-    deck_links = get_deck_links('https://yugiohtopdecks.com/decklists')
-    deck_data = [get_deck_list('https://yugiohtopdecks.com' + link) for link in deck_links]
-    print("Finished scraping deck data.")
+# Scrape deck data
+print("Starting to scrape deck data...")
+deck_links = get_deck_links('https://yugiohtopdecks.com/decklists')
+deck_data = [get_deck_list('https://yugiohtopdecks.com' + link) for link in deck_links]
+print("Finished scraping deck data.")
+
+# Flatten deck data
+flattened_deck_data = [flatten_deck_data(deck, df) for deck in deck_data]
+
+# Display the first 10 rows of the new dataset
+for i, deck_df in enumerate(flattened_deck_data):
+    print(f"First 10 rows of deck {i + 1}:")
+    print(deck_df.head(10))
--- pandas_data_handle.py
+++ pandas_data_handle.py
@@ -16,3 +16,18 @@ def format_numeric_columns(df, numeric_columns):
     for column in numeric_columns:
         df[column] = df[column].apply(lambda x: f"{int(x):04d}" if pd.notnull(pd.to_numeric(x, errors='coerce')) else x)
     return df
+
+def flatten_deck_data(deck_dict, all_card_data):
+    print("Starting to format deck data...")
+    flattened_data = []
+    for deck_part, card_list in deck_dict.items():
+        for card in card_list:
+            card_quantity, card_name = card
+            card_details = all_card_data[all_card_data['name'] == card_name]
+            if not card_details.empty:
+                card_details = card_details.to_dict('records')[0]
+                card_details['deck_part'] = deck_part
+                card_details['quantity'] = card_quantity
+                flattened_data.append(card_details)
+    print("Finished formatting deck data.")
+    return pd.DataFrame(flattened_data)
