e53d33bce1950f9f182ef6ad02fefd1a6e7adf33
Matt Mortensen <mtt.mortensen527@gmail.com>
2023-06-25 17:50:41 -0600

    fixed lvl5 < drawing bug

--- game/deck.py
+++ game/deck.py
@@ -6,6 +6,7 @@ from config import get_db_config  # Importing the function
 class Deck:
     def __init__(self):
         self.cards = self.load_cards_from_db()
+        self.shuffle()
 
     def load_cards_from_db(self):
         # Connect to your MySQL database and fetch the normal monster cards
@@ -18,17 +19,36 @@ class Deck:
         )
 
         cursor = db.cursor()
+        
+        # This is just for testing. I will uncomment below once done. 
+        # First, select one monster of level 5 or higher
+        query_high_level = "SELECT * FROM all_cards WHERE (frameType = 'normal' OR type = 'NormalMonster') AND level >= 5 LIMIT 1"
+        cursor.execute(query_high_level)
+        high_level_card = cursor.fetchone()
 
-        query = "SELECT * FROM all_cards WHERE frameType = 'normal' LIMIT 10"
+        # Then, select up to 9 other normal monsters of any level
+        query_other_cards = "SELECT * FROM all_cards WHERE frameType = 'normal' OR type = 'NormalMonster' AND name != %s LIMIT 9"
+        cursor.execute(query_other_cards, (high_level_card[1],))
+        other_cards = cursor.fetchall()
 
-        cursor.execute(query)
+        # Combine the two sets of cards
+        other_cards = [Card(high_level_card[1], int(high_level_card[3]), high_level_card[12], int(high_level_card[7]), int(high_level_card[8]))] + \
+                      [Card(card[1], int(card[3]), card[12], int(card[7]), int(card[8])) for card in other_cards]
 
-        cards = cursor.fetchall()
+        return other_cards
+
+        #cursor = db.cursor()
+
+        #query = "SELECT * FROM all_cards WHERE frameType = 'normal' LIMIT 10"
+
+        #cursor.execute(query)
+
+        #cards = cursor.fetchall()
 
         # Convert the tuples into Card objects
-        cards = [Card(card[1], int(card[3]), card[12], int(card[7]), int(card[8])) for card in cards]
+        #cards = [Card(card[1], int(card[3]), card[12], int(card[7]), int(card[8])) for card in cards]
 
-        return cards
+        #return cards
 
     def shuffle(self):
         random.shuffle(self.cards)
