0bf05ac576c80e37d42e8c497a4116a011f88447
Matt Mortensen <mtt.mortensen527@gmail.com>
2023-06-25 00:59:19 -0600

    adding card instance(s) to the deck

--- game/deck.py
+++ game/deck.py
@@ -1,6 +1,7 @@
 import mysql.connector
-from config import get_db_config  # Importing the function
 import random
+from card import Card
+from config import get_db_config  # Importing the function
 
 class Deck:
     def __init__(self):
@@ -24,7 +25,16 @@ class Deck:
 
         cards = cursor.fetchall()
 
-        return cards
+        # Create Card instances for each card
+        card_objects = []
+        for card in cards:
+            card_name = card[1]
+            card_level = int(card[3]) if card[3] is not None else None
+            card_type = card[12]
+            card_object = Card(card_name, card_level, card_type)
+            card_objects.append(card_object)
+
+        return card_objects
 
     def shuffle(self):
         random.shuffle(self.cards)
