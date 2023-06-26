13fe25e2375457aa1108f9ab3faccc85aed952dc
Matt Mortensen <mtt.mortensen527@gmail.com>
2023-06-26 17:12:28 -0600

    summon bugs finished. more game cmmds

--- game/game_commands.py
+++ game/game_commands.py
@@ -7,12 +7,13 @@ def get_user_input(prompt, game=None):
                 print("Thanks for playing!")
                 exit()
         elif user_input.lower() == 'check-field' and game is not None:
-            print_game_state(game)
-            continue
+            check_field(game)
+        elif user_input.lower() == 'check-graveyard' and game is not None:
+            check_graveyard(game)
         else:
             return user_input
 
-def print_game_state(game):
+def check_field(game):
     for player in game.players:
         print(f"{player.name}'s Life Points are: {player.life_points}")
         print(f"{player.name}'s deck size is: {len(player.deck.cards)}")
@@ -23,3 +24,13 @@ def print_game_state(game):
                 print(f"  Monster Zone {i}: {monster.name}, ATK: {monster.atk}, DEF: {monster.defense}, Level: {monster.level} Position: {monster.position}")
             else:
                 print(f"  Monster Zone {i}: Empty")
+
+def check_graveyard(game):
+    for player in game.players:
+        print(f"{player.name}'s Graveyard:")
+        if len(player.graveyard) == 0:
+            print("[]")
+        else:
+            for card in player.graveyard:
+                print(card)
+
