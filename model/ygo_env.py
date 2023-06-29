cd0af784afff4bba39270caa0119d0cfe26fb283
Matt Mortensen <mtt.mortensen527@gmail.com>
2023-06-29 08:14:53 -0600

    step method. intro to player/game values for AI

--- model/ygo_env.py
+++ model/ygo_env.py
@@ -1,6 +1,8 @@
 class YGOEnvironment:
-    def __init__(self):
+    def __init__(self, player, game):
         self.state = None
+        self.player = player
+        self.game = game
         self.reset()
     
     def reset(self):
@@ -15,3 +17,31 @@ class YGOEnvironment:
             'opponent_graveyard': []
         }
         return self.state
+    
+    def step(self, action, player, game):
+        # Update the game state based on the action taken by the AI
+        if action == 'normal':
+            pass
+        elif action == 'tribute':
+            # Code to handle tribute summon
+            pass
+        elif action == 'attack':
+            # Code to handle attack
+            pass
+        elif action == 'set':
+            # Code to handle setting a monster
+            pass
+        elif action == 'end':
+            # Code to handle ending the turn
+            pass
+        elif action == 'check-field':
+            # Code to handle checking the field
+            pass
+        elif action == 'check-graveyard':
+            # Code to handle checking the graveyard
+            pass
+        elif isinstance(action, int) and 0 <= action <= 6:
+            # Code to handle selecting a card or a zone
+            pass
+        else: 
+            raise ValueError(f"Invalid action: {action}")
