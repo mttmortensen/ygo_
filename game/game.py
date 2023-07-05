1d108fa240d57c6a1f5228379d54e6a1045b907a
Matt Mortensen <mtt.mortensen527@gmail.com>
2023-07-05 16:13:37 -0600

    updated Game to show Turn class

--- game/game.py
+++ game/game.py
@@ -1,3 +1,5 @@
+from turn import Turn
+
 class Game:
     def __init__(self):
         self.players = []
@@ -31,54 +33,9 @@ class Game:
                     if zone is not None:
                         zone.can_change_position = True
                         zone.has_changed_position = False
-                print(f"\nIt's {player.name}'s turn.")
-                if player.deck.cards:  # Only execute the Draw Phase if the deck is not empty
-                    self.current_phase = "Draw Phase"  # Update current_phase                  
-                    player.draw_phase()
-                    if len(player.hand) >= 7:  # Check if hand size exceeds 7
-                        player.discard()  # Discard a card
-                else: # Deck out win condition
-                    print(f"{player.name}'s deck is empty. {opponent.name} wins the game!")
-                    self.game_over = True
-                    self.end_game()
-                    break
-                if player.life_points <= 0: # Life Point win condition
-                    print(f"{player.name}'s life points have reached 0. {opponent.name} wins the game! ")
-                    self.end_game()
-                    self.game_over = True
-                    break
-                elif opponent.life_points <= 0:
-                    print(f"{opponent.name}'s life points have reached 0. {player.name} wins the game! ")
-                    self.end_game()
-                    self.game_over = True  
-                    break
-                else:
-                    self.current_phase = "Standby Phase"  # Update current_phase
-                    player.standby_phase()
-                    if self.game_over:
-                        break
-
-                    self.current_phase = "Main Phase 1"  # Update current_phase
-                    player.main_phase_1()
-                    if self.game_over:
-                        break
-
-                    self.current_phase = "Battle Phase"  # Update current_phase
-                    player.battle_phase(opponent, self.turn, self)  # Pass the opponent player as an argument
-                    if self.game_over:
-                        break
-
-                    self.current_phase = "Main Phase 2"  # Update current_phase
-                    player.main_phase_2()
-                    if self.game_over:
-                        break
-
-                    self.current_phase = "End Phase"  # Update current_phase
-                    player.end_phase()
-                    if self.game_over:
-                        break
-
-                    self.turn += 1  # Increment the turn count at the end of each player's turn
+                turn = Turn(player, opponent, self)
+                turn.play_turn()
+                self.turn += 1  # Increment the turn count at the end of each player's turn
             for player in self.players: # Reseting monsters that have attacked  for this turn
                 for zone in player.field.zones[player.name]["main_monster_zones"]:
                     if zone is not None:
